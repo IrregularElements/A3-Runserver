d1df1051f649906bf69870c8ca77d41967d9cc1a
inty <interruptinuse@gmail.com>
2022-11-12 15:02:37 +0000

    Update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -2,10 +2,21 @@
 # Run as the arma user:
 #   ./runserver Server/Vindicta-WW2-Chernarus
 
-set -ex
+DEFAULT_A3_DIR="/data/Arma 3/Arma 3"
+DEFAULT_A3_MODS_DIR="/data/Arma 3/Mods"
+
+set -x
+
+function join_by() {
+  local d="$1"; shift
+  echo -n "$1"
+  shift || return
+  printf "%s" "${@/#/$d}"
+}
 
 atexit() {
   set +e
+#  read
   [[ -n $HEADLESS_PID ]] && kill -- $HEADLESS_PID
   for (( i=${#FUSEMOUNTS[@]}-1 ; i>=0 ; i-- )) ; do
     fusermount -uz -- "${FUSEMOUNTS[i]}"
@@ -16,7 +27,7 @@ trap atexit EXIT
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
-MODDIR="$(readlink -f "$DIR/Mods")"
+MODDIR="${DEFAULT_A3_MODS_DIR}"
 
 SERVERDIR="$1" ; shift 1
 [[ -d Server/$SERVERDIR ]] && SERVERDIR="Server/$SERVERDIR"
@@ -27,7 +38,7 @@ else
   if [[ -e default.txt ]]; then
     ARMADIR="$(readlink -f "$(cat default.txt)")"
   else
-    ARMADIR="${DIR}/1.98.146373"
+    ARMADIR="${DEFAULT_A3_DIR}"
   fi
 fi
 
@@ -36,14 +47,6 @@ export WINEPREFIX=$HOME/.wine_armaserver
 Xvfb $DISPLAY &
 ulimit -n 10240
 wineserver -p1
-
-function join_by() {
-  local d="$1"; shift
-  echo -n "$1"
-  shift || return
-  printf "%s" "${@/#/$d}"
-}
-
 
 readarray -t _SERVERMODS <"$SERVERDIR/servermods.txt"
 for M in "${_SERVERMODS[@]}" ; do
@@ -61,7 +64,9 @@ HEADLESS=
 FUSEMOUNTS=()
 mkdir -p -- "$SERVERDIR/Arma"
 bindfs -o nonempty "$ARMADIR" "$SERVERDIR/Arma" && FUSEMOUNTS+=("$SERVERDIR"/Arma)
+mkdir -p -- "$SERVERDIR/Arma/MPMissions"
 bindfs -o nonempty "$SERVERDIR/MPMissions" "$SERVERDIR/Arma/MPMissions" && FUSEMOUNTS+=("$SERVERDIR/Arma/MPMissions")
+mkdir -p -- "$SERVERDIR/Arma/userconfig"
 bindfs -o nonempty "$SERVERDIR/userconfig" "$SERVERDIR/Arma/userconfig" && FUSEMOUNTS+=("$SERVERDIR/Arma/userconfig")
 
 HEADLESS_PID=
@@ -77,7 +82,7 @@ if [[ -n $HEADLESS ]] ; then
   disown
 fi
 
-wine "$SERVERDIR"/Arma/arma3server_x64.exe \
+wine64 "$SERVERDIR"/Arma/arma3server_x64.exe \
   -nosplash \
   -port=2302 \
   -name=server \
